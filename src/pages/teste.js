eddb805704b3fba5aa9347040bf32cdf41746e34
Caio Nascimento <caionsantos2000@gmail.com>
2021-03-11 08:08:34 -0300

    added material design module

--- src/pages/teste.js
+++ src/pages/teste.js
@@ -1,5 +1,11 @@
 import React from "react"
 
 export default function Teste(){
-  return <h1>Pagina 2</h1>
+  return (
+    <>
+    <h1>Pagina 2</h1>
+    <h3>Conteudo da pagina</h3>  
+  </>
+  )
+  
 }
